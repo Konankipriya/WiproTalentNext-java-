191bfbb13e94404f3c5a3927802729c74e0c0026
Konankipriya <112790443+Konankipriya@users.noreply.github.com>
2024-06-19 14:46:21 +0530

    Create Asciivalues.java

---
+++ arrays/Asciivalues.java
@@ -0,0 +1,11 @@
+class AsciiCharacter
+{
+  public static void main(String[] args)
+  {
+    int[] a=new int[] {65,66,67,68,69,70,71,72,73,74,75};
+    for(int i=0;i<a.length;i++)
+    {
+      System.out.print((char)a[i]+" ");
+    }
+   }
+}
